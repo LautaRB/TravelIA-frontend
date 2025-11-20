fe428b03d13bffd5cdfd6b9cf14185cccc1adfb9
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-11-20 01:13:29 -0300

    Redirección entre Login o Dashboard según el token

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import type { MiddlewareHandler } from "astro";
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  const cookies = context.request.headers.get("cookie") || "";
+  const hasToken = cookies.includes("authToken=");
+  const url = new URL(context.request.url);
+
+  if (url.pathname.startsWith("/dashboard") && !hasToken) {
+    return context.redirect("/login");
+  }
+
+  if (url.pathname === "/login" && hasToken) {
+    return context.redirect("/dashboard");
+  }
+
+  return next();
+};
