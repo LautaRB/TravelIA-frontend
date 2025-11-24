38159c3d66405268bfd41f7f99cb9ee32514c539
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-11-23 23:38:39 -0300

    Cambios para probar en Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.travelia.app',
+  appName: 'TravelIA',
+  webDir: './dist'
+};
+
+export default config;
