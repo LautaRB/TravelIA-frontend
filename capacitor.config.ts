1a788d2bfb8edd2102de92cf8e990badd031d502
Lautaro Rodriguez Brown <lautanba@gmail.com>
2025-12-01 00:08:53 -0300

    Reestructuración de los cambios para Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.travelia.app',
   appName: 'TravelIA',
-  webDir: './dist'
+  webDir: './dist',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '360414482088-rtp2h0ao33g08ovo4fa2q6rk0c2n1aos.apps.googleusercontent.com', 
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
